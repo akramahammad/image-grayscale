8d776e24b7d49481d4a44de0991aa1d70072b5f1
Akram Ahamed <akram.ahammad@gmail.com>
2022-08-18 21:44:22 +0530

    Added changes for grayscaling an image using rust and webassembly

--- public/main.js
+++ public/main.js
@@ -1,1 +1,31 @@
-console.log('test');
+    async function init(){
+        let rustApp=null
+
+    try {
+        rustApp= await import('../pkg')
+    } catch (error) {
+        console.error(error)
+        return
+    }
+    
+    const input=document.getElementById('upload')
+    const fileReader=new FileReader()
+    fileReader.onloadend=() =>{
+        let base64=fileReader.result.replace(
+            /^data:image\/(png|jpeg|jpg);base64,/, ''
+        )
+
+        const imageDataUrl=rustApp.grayscale(base64);
+        console.log('image processed')
+        document.getElementById('new-img').setAttribute(
+            'src', imageDataUrl
+        )
+    }
+    input.addEventListener('change', ()=>{
+        fileReader.readAsDataURL(input.files[0]);
+    })
+
+    }
+
+    init()
+    
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HTMLWebPackPlugin=require('html-webpack-plugin')
+const WasmPlugin=require('@wasm-tool/wasm-pack-plugin')
 
 module.exports={
     entry:'./public/main.js',
@@ -10,6 +11,12 @@ module.exports={
     plugins:[
         new HTMLWebPackPlugin({
             template:'./public/index.html'
+        }),
+        new WasmPlugin({
+            crateDirectory:path.resolve(__dirname,'.')
         })
-    ]
+    ],
+    experiments:{
+        asyncWebAssembly:true
+    }
 }
